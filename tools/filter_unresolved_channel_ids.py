9877debb48f0e393692dd35f41dd9497123b9d58
Boinko <boinko@scrape.exchange>
2026-04-05 10:04:13 +0200

    adding metrics to filter unresolved channels

--- tools/filter_unresolved_channel_ids.py
+++ tools/filter_unresolved_channel_ids.py
@@ -14,20 +14,29 @@ print(f'Found {len(channel_ids)} unique unresolved channel IDs.')
 
 with open('../../byoda/data/channels.lst', 'r') as file_desc:
     existing_channels: set[str] = set()
+    duplicate_channel_ids: int = 0
+    duplicate_channel_names: int = 0
     channel_name: str
-    for line in file_desc:
-        line = line.strip()
-        if not line:
+    for channel_name in file_desc:
+        channel_name = channel_name.strip()
+        if not channel_name:
             continue
-        if ',' in line:
-            channel_name = line.split(',', 1)[-1].strip()
-            existing_channels.add(channel_name)
-        elif line.startswith('uc') and len(line) == 24:
-            channel_name = 'UC' + line[2:]
-            existing_channels.add(channel_name)
-        else:
-            existing_channels.add(line)
+        if ',' in channel_name:
+            channel_name = channel_name.split(',', 1)[-1].strip()
+        elif channel_name.startswith('uc') and len(channel_name) == 24:
+            channel_name = 'UC' + channel_name[2:]
 
+        if channel_name in channel_ids:
+            duplicate_channel_ids += 1
+            continue
+        if channel_name in existing_channels:
+            duplicate_channel_names += 1
+            continue
+        existing_channels.add(channel_name)
+
+print('Existing channels:', len(existing_channels))
+print('Duplicate channel IDs:', duplicate_channel_ids)
+print('Duplicate channel names:', duplicate_channel_names)
 
 with open('/tmp/channels.lst', 'w') as file_desc:
     for channel_name in existing_channels:
